a2a1dbdd1754a8fa0460ea66a2cf9aaaf76335ab
xawbi <gushin.adam@yandex.com>
2024-05-06 06:18:37 +0500

    fix next config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ['localhost', 'http://imagegif.ru', 'https://imagegif.ru']
+    domains: ['localhost', 'imagegif.ru']
   }
 }
 
