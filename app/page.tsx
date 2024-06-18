ffe386ab4837bdcb336aaf186d5e396066fbc25e
xawbi <gushin.adam@yandex.com>
2024-06-18 17:38:46 +0500

    add suspence in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { FileDTO } from "@/api/dto/file.dto";
 import { getPublicFiles } from "@/api/public";
 import { UserDTO } from "@/api/dto/user.dto";
 import { getUser } from "@/api/user";
-import React from "react";
+import React, { Suspense } from "react";
 import DropdownSortBtn from "@/components/DropdownSortBtn";
 import { cookies } from "next/headers";
 import SearchPost from "@/components/public/SearchPost";
@@ -10,21 +10,24 @@ import LoadMore from "@/components/load-more";
 import { checkBan } from "@/api/checkVerify";
 import { redirect } from "next/navigation";
 import Footer from "@/components/footer/Footer";
+import LoadingPublic from "@/components/public/LoadingPublic";
 
 export default async function Page() {
   if (await checkBan()) redirect('/ban')
-  const selectedSortCookie = cookies().get("selectedSortPublic")?.value;
-  let filesPublic: FileDTO[] = await getPublicFiles('', '', selectedSortCookie, 1);
-  const user: UserDTO = await getUser();
+  const selectedSortCookie = cookies().get("selectedSortPublic")?.value
+  let filesPublic: FileDTO[] = await getPublicFiles('', '', selectedSortCookie, 1)
+  const user: UserDTO = await getUser()
 
   return (
     <>
-      <div className="flex m-2 mx-[5px] justify-between">
-        <SearchPost />
-        <DropdownSortBtn selectedSortCookie={selectedSortCookie} pageType="public" />
-      </div>
-      <LoadMore page={'public'} selectedSortCookie={selectedSortCookie} initialFilesPublic={filesPublic} user={user} />
-      <Footer/>
+      <Suspense fallback={<LoadingPublic/>}>
+        <div className="flex m-2 mx-[5px] justify-between">
+          <SearchPost />
+          <DropdownSortBtn selectedSortCookie={selectedSortCookie} pageType="public" />
+        </div>
+        <LoadMore page={'public'} selectedSortCookie={selectedSortCookie} initialFilesPublic={filesPublic} user={user} />
+        <Footer/>
+      </Suspense>
     </>
   );
 }
---
+++ components/public/LoadingPublic.tsx
@@ -0,0 +1,65 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import DropdownSortBtn from "@/components/DropdownSortBtn";
+import React from "react";
+import { cookies } from "next/headers";
+import SearchPost from "@/components/public/SearchPost";
+
+export default function LoadingPublic() {
+  const selectedSortCookie = cookies().get("selectedSortPublic")?.value
+
+  return (
+    <>
+      <div className="flex m-2 mx-[5px] justify-between">
+        <SearchPost />
+        <DropdownSortBtn selectedSortCookie={selectedSortCookie} pageType="public" />
+      </div>
+      <div
+        className="px-1 colums-1 min-[400px]:columns-2 min-[700px]:columns-3 min-[1100px]:columns-4 min-[1600px]:columns-5 gap-2.5">
+        <Skeleton className="w-full mb-3">
+          <div className="inline-block max-w-[100%]" style={{ width: 100, height: 170 }}></div>
+        </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 210 }}></div>
+      </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 150 }}></div>
+      </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 220 }}></div>
+      </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 260 }}></div>
+      </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 300 }}></div>
+      </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 280 }}></div>
+      </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 360 }}></div>
+      </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 160 }}></div>
+      </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 270 }}></div>
+      </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 200 }}></div>
+      </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 150 }}></div>
+      </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 340 }}></div>
+      </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 230 }}></div>
+      </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 380 }}></div>
+      </Skeleton>
+        <Skeleton className="w-full mb-3">
+          <div className="inline-block max-w-[100%]" style={{ width: 100, height: 280 }}></div>
+        </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 150 }}></div>
+      </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 220 }}></div>
+      </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 230 }}></div>
+      </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 310 }}></div>
+      </Skeleton><Skeleton className="w-full mb-3">
+        <div className="inline-block max-w-[100%]" style={{ width: 100, height: 310 }}></div>
+      </Skeleton>
+      </div>
+    </>
+  );
+}
